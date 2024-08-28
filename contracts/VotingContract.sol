6ebda5902aa1dbd3934fe9f6950dfdcf2a36aa5c
ss <kotadiyasaiyam123@gmail.com>
2024-08-29 00:26:57 +0530

    Contract final

--- contracts/VotingContract.sol
+++ contracts/VotingContract.sol
@@ -13,20 +13,20 @@ contract Create {
 
     // candidate start
     struct Candidate {
-        uint256 candidateID;
+        uint256 candidateId;
         string age;
         string name;
         string image;
-        uint256 votecount;
+        uint256 voteCount;
         address _address;
         string ipfs;
     }
     event CandidateCreate(
-        uint256 indexed candidateID,
+        uint256 indexed candidateId,
         string age,
         string name,
         string image,
-        uint256 votecount,
+        uint256 voteCount,
         address _address,
         string ipfs
     );
@@ -34,7 +34,7 @@ contract Create {
     mapping(address => Candidate) public candidates;
     //candidate end
 
-
+    
     // voter start
     struct Voter{
         uint256 voter_voterId;
@@ -62,7 +62,7 @@ contract Create {
 
     // voterend
 
-
+    // --CANDIDATE SECTION--//
     constructor (){
         votingOrganizer=msg.sender;
     }
@@ -72,14 +72,79 @@ contract Create {
         uint256 idNumber=_candidateId.current();
         Candidate storage candidate=candidates[_address];
         candidate.age=_age;
-        candidate.candidateID=idNumber;
+        candidate.candidateId=idNumber;
         candidate.name=_name;
         candidate.image=_image;
-        candidate.votecount=0;
+        candidate.voteCount=0;
         candidate._address=_address;
         candidate.ipfs=_ipfs;
 
         candidateAddress.push(_address);
-        emit CandidateCreate(idNumber, _age, _name, _image, candidate.votecount, _address, _ipfs);
+        emit CandidateCreate(idNumber, _age, _name, _image, candidate.voteCount, _address, _ipfs);
     }
+    function getCandidate() public view returns(address[] memory){
+        return candidateAddress;
+    }
+    function getCandidateLenght() public view returns(uint256){
+        return candidateAddress.length;
+    }
+    function getCandidateData(address _address) public view returns(string memory,string memory,uint256,string memory,uint256,string memory,address){
+        return(
+            candidates[_address].age,
+            candidates[_address].name,
+            candidates[_address].candidateId,
+            candidates[_address].image,
+            candidates[_address].voteCount,
+            candidates[_address].ipfs,
+            candidates[_address]._address
+            );
+    }
+    
+    //--VOTER SECTION--//
+    function voterRight(address _address,string memory _name, string memory _image,string memory _ipfs) public {
+        require(votingOrganizer==msg.sender,"Only organizer can create voter");
+        _voterId.increment();
+        uint256 idNumber=_voterId.current();
+        Voter storage voter=voters[_address];
+        require(voter.voter_allowed==0,"You are Not allowedd to vote or u have already voted");
+        voter.voter_allowed=1;
+        voter.voter_name=_name;
+        voter.voter_image=_image;
+        voter.voter_address=_address;
+        voter.voter_voterId=idNumber;
+        voter.voter_ipfs=_ipfs;
+        voter.voter_vote=1000;
+        voter.voter_voted=false;
+        votersAddress.push(_address);
+        emit VoterCreated(idNumber,_name,_image,_address, voter.voter_allowed, voter.voter_voted, voter.voter_vote,_ipfs);
+    }
+
+    function vote(address _candidateAddress,uint256 _candidateVoteId) external{
+        Voter storage voter=voters[msg.sender];
+        require(!voter.voter_voted,"You have already voted");
+        require(voter.voter_allowed!=0,"You are not allow to vote");
+        voter.voter_voted=true;
+        voter.voter_vote=_candidateVoteId;
+
+        votedVoters.push(msg.sender);
+        candidates[_candidateAddress].voteCount+=voter.voter_allowed;
+    }
+    function getVoterLength() public view returns (uint256){
+        return votersAddress.length;
+    }
+    function getVoterData(address _address) public view returns(uint256,string memory,string memory,address,string memory,uint256,bool){
+        return(
+            voters[_address].voter_voterId,
+            voters[_address].voter_name,
+            voters[_address].voter_address,
+            voters[_address].voter_image,
+            voters[_address].voter_ipfs,
+            voters[_address].voter_allowed,
+            voters[_address].voter_voted
+        );
+    }
+    function getVoterList() public view returns(address[] memory){
+        return votersAddress;
+    }
+
 }
